b625e31c8be9642c9f89e7605438f9ae2a0260dc
nhapHoangKhoi <nhaphk@gmail.com>
2025-07-04 03:48:26 +0700

    Create component Header UI

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,43 @@
+"use client"
+import Link from "next/link"
+import { FaBars } from "react-icons/fa6";
+import { HeaderMenu } from "./HeaderMenu";
+import { useState } from "react";
+
+export const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+  return (
+    <>
+      <header className="bg-[#000071] py-[15px]">
+        <div className="container mx-auto px-[16px]">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="text-white font-[800] sm:text-[28px] text-[20px] lg:flex-none flex-1">
+              ITJobs
+            </Link>
+            <HeaderMenu showMenu={showMenu} />
+            <div className="inline-flex items-center gap-x-[5px] text-white font-[600] sm:text-[16px] text-[12px]">
+              <Link href="#" className="">
+                Login
+              </Link>
+              <span className="">/</span>
+              <Link href="#" className="">
+                Register
+              </Link>
+            </div>
+            <button 
+              onClick={handleShowMenu}
+              className="text-white text-[20px] ml-[12px] lg:hidden inline-block"
+            >
+              <FaBars />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
---
+++ src/app/components/HeaderMenu.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link"
+import { FaAngleDown, FaAngleRight } from "react-icons/fa6";
+
+export const HeaderMenu = (props: {
+  showMenu: boolean
+}) => {
+  const { showMenu } = props;
+
+  return (
+    <>
+      <nav className={"lg:block " + (showMenu ? "fixed top-0 left-0 w-[280px] h-[100vh] z-[999] bg-[#000065]" : "hidden")}>
+        <ul className="flex gap-x-[30px] flex-wrap">
+          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px] relative group/sub-1">
+            <Link href="#" className="font-[600] text-[16px] text-white">
+              Available jobs
+            </Link>
+            <FaAngleDown className="text-white text-[16px]" />
+            <ul className="absolute top-[100%] left-[0] w-[280px] bg-[#000065] hidden group-hover/sub-1:block">
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                <Link href="#" className="font-[600] text-[16px] text-white">
+                  Jobs by technologies
+                </Link>
+                <FaAngleRight className="text-white text-[16px]" />
+                <ul className="absolute top-[0] left-[100%] w-[280px] bg-[#000065] hidden group-hover/sub-2:block">
+                  <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                    <Link href="#" className="font-[600] text-[16px] text-white">
+                      ReactJS
+                    </Link>
+                  </li>
+                  <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                    <Link href="#" className="font-[600] text-[16px] text-white">
+                      NodeJS
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                <Link href="#" className="font-[600] text-[16px] text-white">
+                  Jobs by companies
+                </Link>
+                <FaAngleRight className="text-white text-[16px]" />
+              </li>
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                <Link href="#" className="font-[600] text-[16px] text-white">
+                  Jobs by locations
+                </Link>
+                <FaAngleRight className="text-white text-[16px]" />
+              </li>
+            </ul>
+          </li>
+          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px]">
+            <Link href="#" className="font-[600] text-[16px] text-white">
+              Top Companies
+            </Link>
+            <FaAngleDown className="text-white text-[16px]" />
+          </li>
+          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px]">
+            <Link href="#" className="font-[600] text-[16px] text-white">
+              Recruiters
+            </Link>
+            <FaAngleDown className="text-white text-[16px]" />
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
