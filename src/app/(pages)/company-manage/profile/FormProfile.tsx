d47c588cbb463ceb2854bfd29606ed65ecd8fde1
nhapHoangKhoi <nhaphk@gmail.com>
2025-08-09 14:03:15 +0700

    Integrate TinyMCE Editor - FE

--- src/app/(pages)/company-manage/profile/FormProfile.tsx
+++ src/app/(pages)/company-manage/profile/FormProfile.tsx
@@ -2,7 +2,7 @@
 "use client"
 import { useAuth } from "@/hooks/useAuth"
 import JustValidate from "just-validate";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
@@ -14,6 +14,7 @@ import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 // --- End image preview with filepond
 import { Toaster, toast } from 'sonner';
+import { EditorMCE } from "@/app/components/editor/EditorMCE";
 
 // Register plugin
 registerPlugin(
@@ -26,6 +27,7 @@ export const FormProfile = () => {
   const [logos, setLogos] = useState<any[]>([]);
   const [cityList, setCityList] = useState<any[]>([]);
   const [isValid, setIsValid] = useState(false);
+  const editorRef = useRef(null);
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/city/list`)
@@ -89,7 +91,11 @@ export const FormProfile = () => {
       const workOvertime = event.target.workOvertime.value;
       const email = event.target.email.value;
       const phone = event.target.phone.value;
-      const description = event.target.description.value;
+      
+      let description = "";
+      if (editorRef.current) {
+        description = (editorRef.current as any).getContent();
+      }
 
       let logo = null;
       if(logos.length > 0) {
@@ -265,12 +271,7 @@ export const FormProfile = () => {
             <label htmlFor="description" className="block font-[500] text-[14px] text-black mb-[5px]">
               Description
             </label>
-            <textarea 
-              name="description" 
-              defaultValue={infoCompany.description}
-              id="description" 
-              className="w-[100%] h-[350px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
-            ></textarea>
+            <EditorMCE editorRef={editorRef} value={infoCompany.description} />
           </div>
           <div className="sm:col-span-2">
             <button className="bg-[#0088FF] rounded-[4px] h-[48px] px-[20px] font-[700] text-[16px] text-white cursor-pointer">
