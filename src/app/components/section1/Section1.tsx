c0c6de0d485de0331491057713171398428b23d5
nhapHoangKhoi <nhaphk@gmail.com>
2025-08-10 20:38:42 +0700

    Handle query for search box (home page

--- src/app/components/section1/Section1.tsx
+++ src/app/components/section1/Section1.tsx
@@ -1,21 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export const Section1 = () => {
+  const router = useRouter();
+
+  const handleSearch = (event: any) => {
+    event.preventDefault();
+    const city = event.target.city.value;
+    const keyword = event.target.keyword.value;
+
+    const query = `?city=${encodeURIComponent(city)}&keyword=${encodeURIComponent(keyword)}`;
+    router.push(`/search${query}`);
+  }
+
   return (
     <>
       <div className="bg-[#000065] py-[60px]">
         <div className="container mx-auto px-[16px]">
-          <h1 className="text-white font-[700] text-[28px] text-center mb-[30px]">887 Việc làm IT cho Developer &quot;Chất&quot;</h1>
-          <form action="" className="flex flex-wrap gap-x-[15px] gap-y-[12px] mb-[30px]">
-            <select name="" className="bg-white md:w-[240px] w-[100%] h-[56px] rounded-[4px] px-[20px] font-[500] text-[16px] text-[#121212]">
+          <h1 className="text-white font-[700] text-[28px] text-center mb-[30px]">887 Opening jobs for &quot;Developers&quot;</h1>
+          <form action="" onSubmit={handleSearch} className="flex flex-wrap gap-x-[15px] gap-y-[12px] mb-[30px]">
+            <select name="city" className="bg-white md:w-[240px] w-[100%] h-[56px] rounded-[4px] px-[20px] font-[500] text-[16px] text-[#121212]">
               <option value="">All locations</option>
-              <option value="">Hà Nội</option>
-              <option value="">Đà Nẵng</option>
-              <option value="">Hồ Chí Minh</option>
-              <option value="">Others</option>
+              <option value="Hà Nội">Hà Nội</option>
+              <option value="Đà Nẵng">Đà Nẵng</option>
+              <option value="Hồ Chí Minh">Hồ Chí Minh</option>
             </select>
-            <input type="text" name="" placeholder="Search by keyword..." className="md:flex-1 flex-none w-[100%] bg-white h-[56px] rounded-[4px] px-[20px] font-[500] text-[16px] text-[#121212]" />
+            <input type="text" name="keyword" placeholder="Search by keyword..." className="md:flex-1 flex-none w-[100%] bg-white h-[56px] rounded-[4px] px-[20px] font-[500] text-[16px] text-[#121212]" />
             <button className="bg-[#0088FF] md:w-[240px] w-[100%] h-[56px] rounded-[4px] text-white inline-flex items-center justify-center gap-[10px] font-[500] text-[16px]">
               <FaMagnifyingGlass className="text-[20px]" /> Search
             </button>
@@ -25,13 +38,13 @@ export const Section1 = () => {
               People are searching for:
             </div>
             <div className="flex flex-wrap gap-[10px]">
-              <Link href="#" className="border border-[#414042] bg-[#121212] hover:bg-[#414042] rounded-[20px] inline-block py-[8px] px-[22px] font-[500] text-[16px] text-[#DEDEDE] hover:text-white">
+              <Link href="/search?language=ReactJS" className="border border-[#414042] bg-[#121212] hover:bg-[#414042] rounded-[20px] inline-block py-[8px] px-[22px] font-[500] text-[16px] text-[#DEDEDE] hover:text-white">
                 ReactJS
               </Link>
-              <Link href="#" className="border border-[#414042] bg-[#121212] hover:bg-[#414042] rounded-[20px] inline-block py-[8px] px-[22px] font-[500] text-[16px] text-[#DEDEDE] hover:text-white">
+              <Link href="/search?language=Javascript" className="border border-[#414042] bg-[#121212] hover:bg-[#414042] rounded-[20px] inline-block py-[8px] px-[22px] font-[500] text-[16px] text-[#DEDEDE] hover:text-white">
                 Javascript
               </Link>
-              <Link href="#" className="border border-[#414042] bg-[#121212] hover:bg-[#414042] rounded-[20px] inline-block py-[8px] px-[22px] font-[500] text-[16px] text-[#DEDEDE] hover:text-white">
+              <Link href="/search?language=NodeJS" className="border border-[#414042] bg-[#121212] hover:bg-[#414042] rounded-[20px] inline-block py-[8px] px-[22px] font-[500] text-[16px] text-[#DEDEDE] hover:text-white">
                 NodeJS
               </Link>
             </div>
