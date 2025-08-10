bc7854a52bd050af641fdb48e3b6369531c626a4
nhapHoangKhoi <nhaphk@gmail.com>
2025-08-10 20:21:35 +0700

    Hot fix header menu

--- src/app/components/header/HeaderMenu.tsx
+++ src/app/components/header/HeaderMenu.tsx
@@ -25,36 +25,64 @@ export const HeaderMenu = (props: {
                 <FaAngleRight className="text-white text-[16px]" />
                 <ul className="absolute top-[0] left-[100%] w-[280px] bg-[#000065] hidden group-hover/sub-2:block">
                   <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
-                    <Link href="#" className="font-[600] text-[16px] text-white">
+                    <Link href="/search?language=ReactJS" className="font-[600] text-[16px] text-white">
                       ReactJS
                     </Link>
                   </li>
                   <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
-                    <Link href="#" className="font-[600] text-[16px] text-white">
+                    <Link href="/search?language=NodeJS" className="font-[600] text-[16px] text-white">
                       NodeJS
                     </Link>
                   </li>
                 </ul>
               </li>
-              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
-                <Link href="#" className="font-[600] text-[16px] text-white">
-                  Jobs by companies
-                </Link>
-                <FaAngleRight className="text-white text-[16px]" />
-              </li>
-              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
                 <Link href="#" className="font-[600] text-[16px] text-white">
                   Jobs by locations
                 </Link>
                 <FaAngleRight className="text-white text-[16px]" />
+                <ul className="absolute top-[0] left-[100%] w-[280px] bg-[#000065] hidden group-hover/sub-2:block z-[1]">
+                  <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                    <Link href="/search?city=Hà Nội" className="font-[600] text-[16px] text-white">
+                      Hà Nội
+                    </Link>
+                  </li>
+                  <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                    <Link href="/search?city=Đà Nẵng" className="font-[600] text-[16px] text-white">
+                      Đà Nẵng
+                    </Link>
+                  </li>
+                  <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                    <Link href="/search?city=Hồ Chí Minh" className="font-[600] text-[16px] text-white">
+                      Hồ Chí Minh
+                    </Link>
+                  </li>
+                </ul>
               </li>
             </ul>
           </li>
-          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px] cursor-pointer">
+          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px] relative group/sub-1 cursor-pointer">
             <Link href="#" className="font-[600] text-[16px] text-white">
               Top Companies
             </Link>
             <FaAngleDown className="text-white text-[16px]" />
+            <ul className="absolute top-[100%] left-[0] w-[280px] bg-[#000065] hidden group-hover/sub-1:block">
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                <Link href="/search?company=ABC" className="font-[600] text-[16px] text-white">
+                  ABC
+                </Link>
+              </li>
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                <Link href="/search?company=DEF" className="font-[600] text-[16px] text-white">
+                  DEF
+                </Link>
+              </li>
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                <Link href="/search?company=XYZ" className="font-[600] text-[16px] text-white">
+                  XYZ
+                </Link>
+              </li>
+            </ul>
           </li>
           
           {!isLogin && (
