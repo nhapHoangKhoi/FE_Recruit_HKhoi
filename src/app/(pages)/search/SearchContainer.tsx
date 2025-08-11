e1adaa11e55bc8347038ee6b13a18017614f5df7
nhapHoangKhoi <nhaphk@gmail.com>
2025-08-11 10:42:31 +0700

    Filter by technologies - FE

---
+++ src/app/(pages)/search/SearchContainer.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
+import { CardJobItem } from "@/app/components/card/CardJobItem"
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react";
+
+export const SearchContainer = () => {
+  const searchParams = useSearchParams();
+  const language = searchParams.get("language") || "";
+  const [jobList, setJobList] = useState<any[]>([]);
+  
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/search?language=${language}`)
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == "success") {
+          setJobList(data.jobs);
+        }
+      })
+  }, [language]);
+
+  return (
+    <>
+      <div className="py-[60px]">
+        <div className="container mx-auto px-[16px]">
+          <h2 className="font-[700] text-[28px] text-[#121212] mb-[30px]">
+            {jobList.length} Opening jobs for <span className="text-[#0088FF]">{language}</span>
+          </h2>
+
+          <div 
+            className="bg-white rounded-[8px] py-[10px] px-[20px] mb-[30px] flex flex-wrap gap-[12px]"
+            style={{
+              boxShadow: "0px 4px 20px 0px #0000000F"
+            }}
+          >
+            <select name="" className="border border-[#DEDEDE] rounded-[20px] h-[36px] px-[18px] font-[400] text-[16px] text-[#414042]">
+              <option value="">Job types</option>
+              <option value="">Intern</option>
+              <option value="">Fresher</option>
+              <option value="">Junior</option>
+              <option value="">Middle</option>
+              <option value="">Senior</option>
+              <option value="">Manager</option>
+            </select>
+            <select name="" className="border border-[#DEDEDE] rounded-[20px] h-[36px] px-[18px] font-[400] text-[16px] text-[#414042]">
+              <option value="">Workplace type</option>
+              <option value="">Onsite</option>
+              <option value="">Remote</option>
+              <option value="">Hybrid</option>
+            </select>
+          </div>
+
+          <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[20px]">
+            {jobList.map(item => (
+              <CardJobItem key={item.id} item={item} />
+            ))}
+          </div>
+
+          <div className="mt-[30px]">
+            <select name="" className="border border-[#DEDEDE] rounded-[8px] py-[12px] px-[18px] font-[400] text-[16px] text-[#414042]">
+              <option value="">Trang 1</option>
+              <option value="">Trang 2</option>
+              <option value="">Trang 3</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
