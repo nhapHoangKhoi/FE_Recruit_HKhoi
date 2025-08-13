0d06b045519c50909e387381a12a0394810dfafb
nhapHoangKhoi <nhaphk@gmail.com>
2025-08-13 11:30:05 +0700

    Build manage resumes page - FE

--- src/app/(pages)/company-manage/cv/list/CVList.tsx
+++ src/app/(pages)/company-manage/cv/list/CVList.tsx
@@ -1,25 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @next/next/no-img-element */
-import { Metadata } from "next"
+"use client"
+import { cvStatusList } from "@/config/cvList"
+import { workingFormList } from "@/config/workingForm"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 import { FaBriefcase, FaCircleCheck, FaEnvelope, FaEye, FaPhone, FaUserTie } from "react-icons/fa6"
-import { CVList } from "./CVList"
 
-export const metadata: Metadata = {
-  title: "Manage Resumes",
-  description: "Description Manage Resumes...",
-}
+export const CVList = () => {
+  const [listCVs, setListCVs] = useState<any[]>([]);
 
-export default function CompanyManageCVListPage() {
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/company/resume/list`, {
+      method: "GET",
+      credentials: "include"
+    })
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == "success") {
+          setListCVs(data.listResumes);
+        }
+      })
+  }, []);
+
   return (
     <>
-      <div className="py-[60px]">
-        <div className="container mx-auto px-[16px]">
-          <h2 className="font-[700] sm:text-[28px] text-[24px] sm:w-auto w-[100%] text-[#121212] mb-[20px]">
-            Resumes Management
-          </h2>
-          <CVList />
-          {/* <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[20px]">
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[20px]">
+        {listCVs.map(item => {
+          const workingForm = workingFormList.find(work => work.value === item.jobWorkingForm)?.label;
+          const status = cvStatusList.find(itemStatus => itemStatus.value === item.status);
+
+          return (
             <div 
+              key={item.id}
               className="border border-[#DEDEDE] rounded-[8px] flex flex-col relative truncate"
               style={{
                 background: "linear-gradient(180deg, #F6F6F6 2.38%, #FFFFFF 70.43%)"
@@ -31,31 +44,39 @@ export default function CompanyManageCVListPage() {
                 className="absolute top-[0px] left-[0px] w-[100%] h-auto" 
               />
               <h3 className="mt-[20px] mx-[16px] font-[700] text-[18px] text-[#121212] text-center flex-1 whitespace-normal line-clamp-2">
-                Frontend Engineer (ReactJS)
+                {item.jobTitle}
               </h3>
               <div className="mt-[12px] text-center font-[400] text-[14px] text-black">
-                Applicant: <span className="font-[700]">Lê Văn A</span>
+                Applicant: <span className="font-[700]">{item.fullName}</span>
               </div>
               <div className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] text-[#121212]">
-                <FaEnvelope className="" /> levana@gmail.com
+                <FaEnvelope className="" /> {item.email}
               </div>
               <div className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] text-[#121212]">
-                <FaPhone className="" /> 0123456789
+                <FaPhone className="" /> {item.phone}
               </div>
               <div className="mt-[12px] text-center font-[600] text-[16px] text-[#0088FF]">
-                1.000$ - 1.500$
+                {item.jobSalaryMin.toLocaleString("vi-VN")}$ - {item.jobSalaryMax.toLocaleString("vi-VN")}$
               </div>
               <div className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] text-[#121212]">
-                <FaUserTie className="text-[16px]" /> Fresher
+                <FaUserTie className="text-[16px]" /> {item.jobPosition}
               </div>
               <div className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] text-[#121212]">
-                <FaBriefcase className="text-[16px]" /> Onsite
+                <FaBriefcase className="text-[16px]" /> {workingForm}
               </div>
-              <div className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] text-[#FF0000]">
-                <FaEye className="text-[16px]" /> Unread
+              <div className={
+                "mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] " +
+                (item.viewed ? "" : "text-[#FF0000]")
+              }>
+                <FaEye className="text-[16px]" /> {item.viewed ? "Viewed" : "Unread"}
               </div>
-              <div className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px] text-[#121212]">
-                <FaCircleCheck className="text-[16px]" /> Chưa duyệt
+              <div 
+                className="mt-[6px] flex justify-center items-center gap-[8px] font-[400] text-[14px]"
+                style={{
+                  color: status?.color
+                }}
+              >
+                <FaCircleCheck className="text-[16px]" /> {status?.label}
               </div>
               <div className="flex flex-wrap items-center justify-center gap-[8px] mt-[12px] mb-[20px] mx-[10px]">
                 <Link href="#" className="bg-[#0088FF] rounded-[4px] font-[400] text-[14px] text-white inline-block py-[8px] px-[20px]">
@@ -72,8 +93,16 @@ export default function CompanyManageCVListPage() {
                 </Link>
               </div>
             </div>
-          </div> */}
-        </div>
+          )
+        })}
+      </div>
+
+      <div className="mt-[30px]">
+        <select name="" className="border border-[#DEDEDE] rounded-[8px] py-[12px] px-[18px] font-[400] text-[16px] text-[#414042]">
+          <option value="">Page 1</option>
+          <option value="">Page 2</option>
+          <option value="">Page 3</option>
+        </select>
       </div>
     </>
   )
---
+++ src/config/cvList.ts
@@ -0,0 +1,17 @@
+export const cvStatusList = [
+  {
+    label: "Initial",
+    value: "initial",
+    color: "#121212"
+  },
+  {
+    label: "Accepted",
+    value: "accepted",
+    color: "#47BE02"
+  },
+  {
+    label: "Rejected",
+    value: "rejected",
+    color: "#FF5100"
+  }
+];
